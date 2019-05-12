ef5d7aeedcd358bd5eaeb71f61c8257e223a9067
Christopher Heppell <chris.john.heppell@gmail.com>
2019-05-12 15:29:38 +0100

    show hour of day in weather report screen

--- pi-commute-dashboard/unicorn/show_weather.py
+++ pi-commute-dashboard/unicorn/show_weather.py
@@ -5,9 +5,13 @@ except ImportError:
     from unicorn_hat_sim import unicornhathd as unicorn
 from transport import TrainStatus
 import time
+from datetime import datetime
 
 from .icons import get as get_icon
 from .icons import print_icon
+
+from .digits import get as get_digit
+from .digits import print_digit
 
 from weather import WeatherCondition
 
@@ -62,6 +66,7 @@ def show_weather_report(weather_report, is_return):
         print_icon(unicorn, matrix, start_x, start_y, r1, g1, b1, r2, g2, b2, r3, g3, b3)
 
     show_direction(is_return)
+    show_time(weather_report.time)
 
     unicorn.show()
     time.sleep(5)
@@ -69,7 +74,7 @@ def show_weather_report(weather_report, is_return):
 
 def show_direction(is_return):
     start_y = u_height - 10 - 1
-    start_x = 5
+    start_x = 1
 
     if is_return:
         r1, g1, b1 = (0, 0, 255)
@@ -83,3 +88,15 @@ def show_direction(is_return):
         r3, g3, b3 = (r1, g1, b1)
         matrix = get_icon('small_arrow_up')
         print_icon(unicorn, matrix, start_x, start_y, r1, g1, b1, r2, g2, b2, r3, g3, b3)
+
+def show_time(timestamp):
+    start_y = u_height - 10 - 1
+    start_x = 5 + 1 + 1
+
+    hour = datetime.utcfromtimestamp(timestamp).strftime('%H')
+
+    r, g, b = (255, 255, 255)
+    for digit in list(hour):
+        matrix = get_digit(digit)
+        print_digit(unicorn, matrix, start_x, start_y, r, g, b)
+        start_x = start_x + 4
